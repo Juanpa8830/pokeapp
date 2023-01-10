e826548722e5c71148a78ebe57c872c2be49276a
PrangonGhose <prangonghose@gmail.com>
2023-01-11 03:21:38 +0600

    Add Involvement API class

---
+++ src/modules/involvementAPI.js
@@ -0,0 +1,49 @@
+const url = 'http://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/rneS550dVMdEKWuisWBb/';
+
+export default class InvolvementAPI {
+  static getComments = async (itemId) => {
+    const response = await fetch(`${url}comments?item_id=${itemId}`);
+    const data = await response.json();
+    if (data.error === undefined) {
+      return data;
+    }
+    return data.error.status;
+  };
+
+  static postComment = async (itemId, username, comment) => {
+    try {
+      const params = {
+        item_id: itemId,
+        username: username,
+        comment: comment,
+      };
+      const response = await fetch(`${url}comments`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(params),
+      });
+      return response.status;
+    } catch (err) {
+      return err;
+    }
+  };
+
+  static getLikes = async () => {
+    const responseponse = await fetch(`${url}likes`);
+    const data = await responseponse.json();
+    return data;
+  };
+
+  static postLike = async (id) => {
+    const response = await fetch(`${url}likes`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ item_id: id }),
+    });
+    return response.status;
+  }
+}
