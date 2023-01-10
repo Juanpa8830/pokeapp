0336b2b586a0634f27d5c966b9eb54d77526a08c
Juanpa8830 <jp3_4@hotmail.com>
2023-01-10 12:55:20 -0500

    Fix webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -3,41 +3,42 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: {
-    index: './src/index.js',
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-  },
+  entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
+      title: 'Output Management',
       template: './src/index.html',
     }),
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    assetModuleFilename: 'assets/[name][ext]',
     clean: true,
-  },
-  optimization: {
-    runtimeChunk: 'single',
   },
   module: {
     rules: [
       {
-        test: /\.css$/i,
+        test: /\.css/,
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(jpg|png|svg|gif)$/,
         type: 'asset/resource',
       },
       {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        test: /\.html$/i,
+        loader: 'html-loader',
       },
     ],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 4000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
   },
 };
