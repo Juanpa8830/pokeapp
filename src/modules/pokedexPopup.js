e0857b74bb57e34b429f4e2c5fd29ad086ba5aba
PrangonGhose <prangonghose@gmail.com>
2023-01-11 18:09:50 +0600

    Add comment posting & screen update functionality

--- src/modules/pokedexPopup.js
+++ src/modules/pokedexPopup.js
@@ -1,11 +1,7 @@
 import getData from './getData.js';
 import popupGenerator from './popupGenerator.js';
-import likeCounter from './likeCounter.js';
-import InvolvementAPI from './involvementAPI.js';
 import commentCounter from './commentCounter.js';
 import pokemonData from './pokemondata.js';
-
-const popup = document.querySelector('.popup');
 
 const url = 'https://pokeapi.co/api/v2/ability/';
 
@@ -13,10 +9,11 @@ export default async function pokedexPopup(id) {
   const api = new pokemonData();
   const pokemons = await api.getmultiplePokemon(1, 24);
   const data = await getData(`${url}${id}`);
+  const allComments = await commentCounter(id);
   const description = data.effect_entries[1].effect;
   for (let i = 0; i < pokemons.length; i += 1) {
     if (pokemons[i].id === parseInt(id)) {
-      popupGenerator(pokemons[i].image, pokemons[i].name, description);
+      popupGenerator(pokemons[i].image, pokemons[i].name, description, allComments);
       break;
     }
   }
