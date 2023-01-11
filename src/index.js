92b663d6083003d8dd658d345131f4ae8877df69
PrangonGhose <prangonghose@gmail.com>
2023-01-11 20:34:09 +0600

    Update index.js to show live update of likes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import './style.css';
 import start from './modules/start.js';
 import InvolvementAPI from './modules/involvementAPI.js';
+import likeCounter from './modules/likeCounter.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   start();
 })
 
 const cardList = document.getElementById('pokeList');
-cardList.addEventListener('click', (e) => {
+cardList.addEventListener('click', async (e) => {
   if (e.target.classList.contains('likes')) {
-    InvolvementAPI.postLike(e.target.id);
+    await InvolvementAPI.postLike(e.target.id);
   }
+  const nLikes = await likeCounter(e.target.id);
+  const showLike = document.getElementById(e.target.id);
+  showLike.innerHTML = `Likes (${nLikes})`;
 });
