68307e35f26c2b796f190d213cee707182b6c256
PrangonGhose <prangonghose@gmail.com>
2023-01-12 17:35:47 +0600

    Use comment counter function

--- src/modules/pokedexPopup.js
+++ src/modules/pokedexPopup.js
@@ -1,18 +1,18 @@
 import getData from './getData.js';
 import popupGenerator from './popupGenerator.js';
 import commentCounter from './commentCounter.js';
-import pokemonData from './pokemondata.js';
+import PokemonData from './pokemondata.js';
 
 const url = 'https://pokeapi.co/api/v2/ability/';
 
 export default async function pokedexPopup(id) {
-  const api = new pokemonData();
+  const api = new PokemonData();
   const pokemons = await api.getmultiplePokemon(1, 24);
   const data = await getData(`${url}${id}`);
   const allComments = await commentCounter(id);
   const description = data.effect_entries[1].effect;
   for (let i = 0; i < pokemons.length; i += 1) {
-    if (pokemons[i].id === parseInt(id)) {
+    if (pokemons[i].id === parseInt(id, 10)) {
       popupGenerator(pokemons[i].image, pokemons[i].name, description, allComments);
       break;
     }
