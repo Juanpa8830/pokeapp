56125f0cd1911e1fca657edd9ddb36e3c5d8a374
Juanpa8830 <jp3_4@hotmail.com>
2023-01-11 19:13:39 -0500

    Test countItems function

---
+++ src/countItems.test.js
@@ -0,0 +1,29 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import AllPokesCounter from './modules/pokemonCounter.js';
+
+test ('pokeCounter.innerText is equals to 15', () => {
+    // Arrange - acomodar todo para probar
+    document.body.innerHTML = `
+        <div>
+        <div id="poke-cards-cont"></div>
+        <li id="pokedex">Pokedex</li>
+        </div>`;
+
+    const pokeCont = document.getElementById('poke-cards-cont');
+    let htmlCards = '';
+
+    for (let i = 0; i <= 20; i += 1) {
+      htmlCards += '<div class="pokecard"></div>';
+    }
+    pokeCont.innerHTML = htmlCards;
+
+    // Action - ejecutar la funcion que estoy probando
+    AllPokesCounter();
+
+    // Assert - comparar si lo esperado es correcto
+    const counter2 = document.getElementById('pokedex');
+    expect(counter2.innerHTML).toStrictEqual('Pokedex(20)');
+  });
