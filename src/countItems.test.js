090e7cf4622b35e6cde63766fcf6be76e77b4ec0
PrangonGhose <prangonghose@gmail.com>
2023-01-12 17:34:15 +0600

    Add test from collaborator

--- src/countItems.test.js
+++ src/countItems.test.js
@@ -4,26 +4,26 @@
 
 import AllPokesCounter from './modules/pokemonCounter.js';
 
-test ('pokeCounter.innerText is equals to 15', () => {
-    // Arrange - acomodar todo para probar
-    document.body.innerHTML = `
+test('pokeCounter.innerText is equals to 15', () => {
+  // Arrange - acomodar todo para probar
+  document.body.innerHTML = `
         <div>
         <div id="poke-cards-cont"></div>
         <li id="pokedex">Pokedex</li>
         </div>`;
 
-    const pokeCont = document.getElementById('poke-cards-cont');
-    let htmlCards = '';
+  const pokeCont = document.getElementById('poke-cards-cont');
+  let htmlCards = '';
 
-    for (let i = 1; i <= 20; i += 1) {
-      htmlCards += '<div class="pokecard"></div>';
-    }
-    pokeCont.innerHTML = htmlCards;
+  for (let i = 1; i <= 20; i += 1) {
+    htmlCards += '<div class="pokecard"></div>';
+  }
+  pokeCont.innerHTML = htmlCards;
 
-    // Action - ejecutar la funcion que estoy probando
-    AllPokesCounter();
+  // Action - ejecutar la funcion que estoy probando
+  AllPokesCounter();
 
-    // Assert - comparar si lo esperado es correcto
-    const counter2 = document.getElementById('pokedex');
-    expect(counter2.innerHTML).toStrictEqual('Pokedex(20)');
-  });
+  // Assert - comparar si lo esperado es correcto
+  const counter2 = document.getElementById('pokedex');
+  expect(counter2.innerHTML).toStrictEqual('Pokedex(20)');
+});
